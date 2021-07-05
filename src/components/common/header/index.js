81dcfd257a8dad34fbc76cad59f3292aa33c6c20
shuvamk <kshuvam564@gmail.com>
2021-07-05 12:53:28 +0530

    Fixed header css

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./header.css";
 function Header() {
-  const [tab, setTab] = useState("All Banks");
+  const tabName = window.location.pathname.substring(1);
+  const [tab, setTab] = useState(tabName);
   return (
     <div className="header">
       <label className="logo">Bankroww</label>
       <div className="header-menu">
         <Link to="/all-banks">
           <label
-            className={`menu-option ${tab === "All Banks" && "active-tab"}`}
-            onClick={() => setTab("All Banks")}
+            className={`menu-option ${tab === "all-banks" && "active-tab"}`}
+            onClick={() => setTab("all-banks")}
           >
             All Banks
           </label>
         </Link>
         <Link to="/favourites">
           <label
-            className={`menu-option ${tab === "Favourites" && "active-tab"}`}
-            onClick={() => setTab("Favourites")}
+            className={`menu-option ${tab === "favourites" && "active-tab"}`}
+            onClick={() => setTab("favourites")}
           >
             Favourites
           </label>
